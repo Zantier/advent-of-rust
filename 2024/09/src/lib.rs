d8ebac29c2ef40aad52d71ba12912e4e26257d29
David Phillips <zantier.tasa@gmail.com>
2025-05-12 15:26:21 +0100

    Solve problem 09

--- 2024/09/src/lib.rs
+++ 2024/09/src/lib.rs
@@ -17,6 +17,7 @@ impl SnowLb {
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Snowball(pub i64);
 
 impl Snowball {
@@ -27,6 +28,35 @@ impl Snowball {
 
 impl From<SnowKg> for Snowball {
     // 1. Implement the conversion from SnowKg to Snowball
+    fn from(SnowKg(kg): SnowKg) -> Self {
+        let snowballs_float = kg / SNOWBALL_WEIGHT_KG;
+        Self(snowballs_float.round() as i64)
+    }
 }
 
 // 2. Implement the same for SnowLb
+impl From<SnowLb> for Snowball {
+    fn from(SnowLb(lb): SnowLb) -> Self {
+        let snowballs_float = lb / SNOWBALL_WEIGHT_LB;
+        Self(snowballs_float.round() as i64)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{SnowKg, SnowLb, Snowball};
+
+    #[test]
+    fn test_kg() {
+        assert_eq!(Snowball::from(SnowKg(0.49)), Snowball(2));
+        assert_eq!(Snowball::from(SnowKg(0.5)), Snowball(3));
+        assert_eq!(Snowball::from(SnowKg(0.51)), Snowball(3));
+    }
+
+    #[test]
+    fn test_lb() {
+        assert_eq!(Snowball::from(SnowLb(1.102)), Snowball(2));
+        assert_eq!(Snowball::from(SnowLb(1.1025)), Snowball(3));
+        assert_eq!(Snowball::from(SnowLb(1.103)), Snowball(3));
+    }
+}
