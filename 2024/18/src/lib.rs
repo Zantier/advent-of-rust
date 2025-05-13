93234b86f497f22dedc42802ce2f12257cd80f71
David Phillips <zantier.tasa@gmail.com>
2025-05-13 08:52:58 +0100

    Solve problem 18

--- 2024/18/src/lib.rs
+++ 2024/18/src/lib.rs
@@ -1,3 +1,4 @@
+#[derive(Clone)]
 pub struct Sleigh {
     color: String,
     engine: String,
@@ -8,10 +9,51 @@ pub struct Sleigh {
 #[derive(Clone)]
 pub struct SleighBuilder {
     // Define the fields of SleighBuilder here
+    sleigh: Sleigh
 }
 
 impl SleighBuilder {
     // Your code here...
+    pub fn new() -> Self {
+        Self {
+            sleigh: Sleigh {
+                color: "red".to_string(),
+                engine: "reindeer-powered".to_string(),
+                gift_capacity: 100,
+                magical_enhancements: false
+            }
+        }
+    }
+
+    pub fn color(mut self, color: &str) -> Self {
+        self.sleigh.color = color.to_string();
+        self
+    }
+
+    pub fn engine(mut self, engine: &str) -> Self {
+        self.sleigh.engine = engine.to_string();
+        self
+    }
+
+    pub fn gift_capacity(mut self, gift_capacity: u32) -> Self {
+        self.sleigh.gift_capacity = gift_capacity;
+        self
+    }
+
+    pub fn magical_enhancements(mut self) -> Self {
+        self.sleigh.magical_enhancements = true;
+        self
+    }
+
+    pub fn build(self) -> Sleigh {
+        self.sleigh
+    }
+}
+
+impl Default for SleighBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 // Don't Change this implementation
@@ -45,5 +87,5 @@ pub fn main() {
     assert_eq!(sleigh.color(), "gold");
     assert_eq!(sleigh.engine(), "magic");
     assert_eq!(sleigh.gift_capacity(), 350);
-    assert_eq!(sleigh.magical_enhancements(), true);
+    assert!(sleigh.magical_enhancements());
 }
