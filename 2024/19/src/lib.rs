8fb107680c2f8557f5acebec53800d1a8780f30a
David Phillips <zantier.tasa@gmail.com>
2025-05-13 09:33:07 +0100

    Solve problem 19

--- 2024/19/src/lib.rs
+++ 2024/19/src/lib.rs
@@ -1,10 +1,47 @@
+use std::marker::PhantomData;
+
 // 1. We have 3 states:
 // - Empty
 // - Ready
 // - Flying
+pub struct Empty;
+pub struct Ready;
+pub struct Flying;
 
 // 2. Finish the Seligh struct definition
-pub struct Sleigh
-
+pub struct Sleigh<T> {
+    phantom: PhantomData<T>,
+}
 
 // 3. Write the Sleigh Implementations for all states
+impl Sleigh<Empty> {
+    pub fn new() -> Sleigh<Empty> {
+        Sleigh { phantom: PhantomData }
+    }
+
+    pub fn load(self) -> Sleigh<Ready> {
+        Sleigh { phantom: PhantomData }
+    }
+}
+
+impl Default for Sleigh<Empty> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Sleigh<Ready> {
+    pub fn take_off(self) -> Sleigh<Flying> {
+        Sleigh { phantom: PhantomData }
+    }
+
+    pub fn unload(self) -> Sleigh<Empty> {
+        Sleigh { phantom: PhantomData }
+    }
+}
+
+impl Sleigh<Flying> {
+    pub fn land(self) -> Sleigh<Ready> {
+        Sleigh { phantom: PhantomData }
+    }
+}
