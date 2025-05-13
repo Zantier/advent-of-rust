af3527ecf101fb75e6c8211eb256808227ef6b8d
David Phillips <zantier.tasa@gmail.com>
2025-05-13 08:00:32 +0100

    Solve problem 17

--- 2024/17/src/lib.rs
+++ 2024/17/src/lib.rs
@@ -3,6 +3,34 @@
 const CHRISTMAS_EMOJIS: [char; 4] = ['🎅', '🤶', '🎄', '🎁'];
 
 // Your Solution here ...
+pub trait StringExt {
+    fn anonymize_email(&self) -> Self;
+}
+
+impl StringExt for String {
+    fn anonymize_email(&self) -> Self {
+        match self.find("@") {
+            Some(index) => self[..index].to_christmas_emoji() + &self[index..],
+            None => self.to_christmas_emoji(),
+        }
+    }
+}
+
+pub trait StrExt {
+    fn to_christmas_emoji(&self) -> String;
+}
+
+impl StrExt for str {
+    fn to_christmas_emoji(&self) -> String {
+        let mut result = String::new();
+        for i in 0..self.chars().count() {
+            let emoji_index = i % CHRISTMAS_EMOJIS.len();
+            result.push(CHRISTMAS_EMOJIS[emoji_index]);
+        }
+
+        result
+    }
+}
 
 pub fn main() {
     let emails = vec![
@@ -21,5 +49,16 @@ pub fn main() {
     for email in emails {
         let anonymized_email = email.anonymize_email(); // This is the API that Santa wants!
         println!("Original: {} -> Anonymized: {}", email, anonymized_email);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::StringExt;
+
+    #[test]
+    fn test_anonymize() {
+        assert_eq!("santa".to_string().anonymize_email(), "🎅🤶🎄🎁🎅");
+        assert_eq!("santa@north.pole".to_string().anonymize_email(), "🎅🤶🎄🎁🎅@north.pole");
     }
 }
