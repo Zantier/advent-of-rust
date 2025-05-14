f0cf6765428b1d6b6b040d830ad5dbc5c24c603c
David Phillips <zantier.tasa@gmail.com>
2025-05-14 01:04:28 +0100

    Solve problem 23

--- 2024/23/src/lib.rs
+++ 2024/23/src/lib.rs
@@ -1,19 +1,52 @@
+use std::collections::HashMap;
+
 pub struct SantaList {
     // 1. Define the records field
+    records: HashMap<String, bool>
 }
 
 impl SantaList {
     // 2. Implement the new method
+    pub fn new() -> Self {
+        Self { records: HashMap::new() }
+    }
 
     // 3. Implement the add method
+    pub fn add(&mut self, name: &str, behavior: bool) {
+        self.records.insert(name.to_string(), behavior);
+    }
 
     // 4. Implement the remove method
+    pub fn remove(&mut self, name: &str) {
+        self.records.remove(name);
+    }
 
     // 5. Implement the get method
+    pub fn get(&self, name: &str) -> Option<bool> {
+        self.records.get(name).cloned()
+    }
 
     // 6. Implement the count method
+    pub fn count(&self) -> (usize, usize) {
+        (
+            self.list_by_behavior(true).len(),
+            self.list_by_behavior(false).len(),
+        )
+    }
 
     // 7. Implement the list_by_behavior method
+    pub fn list_by_behavior(&self, behavior: bool) -> Vec<String> {
+        self.records.iter()
+            .filter(|&(_name, &val)| val == behavior)
+            .map(|(name, _val)| name.clone())
+            .collect()
+    }
+}
+
+impl Default for SantaList {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 pub fn main() {
@@ -48,4 +81,22 @@ pub fn main() {
         "After removing Bob, Santa's list contains {} nice and {} naughty children.",
         nice, naughty
     );
+}
+
+#[cfg(test)]
+mod test {
+    use crate::SantaList;
+
+    #[test]
+    fn test_add_remove() {
+        let mut list = SantaList::new();
+        list.add("Alice", true);
+        list.add("Bob", false);
+        assert_eq!(list.get("Alice"), Some(true));
+        assert_eq!(list.get("Bob"), Some(false));
+        assert_eq!(list.get("Charlie"), None);
+
+        list.remove("Bob");
+        assert_eq!(list.get("Bob"), None);
+    }
 }
