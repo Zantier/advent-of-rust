567b42812c6f0e9cf3d995589ada83b33e3e27b3
David Phillips <zantier.tasa@gmail.com>
2025-05-14 01:46:59 +0100

    Solve problem 24

--- 2024/24/src/lib.rs
+++ 2024/24/src/lib.rs
@@ -1,4 +1,5 @@
-use std::sync::Arc;
+use std::collections::VecDeque;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 pub trait SleighTask {
@@ -6,23 +7,46 @@ pub trait SleighTask {
 }
 
 pub struct SantaSleighQueue {
-    records: // 1. Should store the tasks
+    // 1. Should store the tasks
+    records: Mutex<VecDeque<Box<dyn SleighTask + Send + Sync>>>,
 }
 
 impl SantaSleighQueue {
     // 2. Define the `new` constructor
+    pub fn new() -> Self {
+        Self { records: Mutex::new(VecDeque::new()) }
+    }
 
     // 3. Define the `enqueue` method
+    pub fn enqueue(&self, task: Box<dyn SleighTask + Send + Sync>) {
+        let mut records = self.records.lock().unwrap();
+        records.push_back(task);
+    }
 
     // 4. Define the `get_task` method
+    pub fn get_task(&self) -> Option<Box<dyn SleighTask + Send + Sync>> {
+        let mut records = self.records.lock().unwrap();
+        records.pop_front()
+    }
+}
+
+impl Default for SantaSleighQueue {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 pub struct ElfTask {
     // 5. Define the fields
+    name: String,
+    urgency: u32,
 }
 
 impl ElfTask {
     // 6. Define the `new` constructor
+    pub fn new(name: &str, urgency: u32) -> Self {
+        ElfTask { name: name.to_string(), urgency }
+    }
 }
 
 impl SleighTask for ElfTask {
@@ -33,10 +57,15 @@ impl SleighTask for ElfTask {
 
 pub struct ReindeerTask {
     // 7. Define the fields
+    name: String,
+    weight: u32,
 }
 
 impl ReindeerTask {
     // 8. Define the `new` constructor
+    pub fn new(name: &str, weight: u32) -> Self {
+        ReindeerTask { name: name.to_string(), weight }
+    }
 }
 
 impl SleighTask for ReindeerTask {
@@ -59,13 +88,11 @@ pub fn main() {
     thread::sleep(std::time::Duration::from_millis(10));
 
     let consumer_queue = Arc::clone(&queue);
-    let consumer = thread::spawn(move || loop {
-        if let Some(task) = consumer_queue.get_task() {
+    let consumer = thread::spawn(move ||
+        while let Some(task) = consumer_queue.get_task() {
             println!("{}", task.describe());
-        } else {
-            break;
         }
-    });
+    );
 
     producer.join().unwrap();
     consumer.join().unwrap();
